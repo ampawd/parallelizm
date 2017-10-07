9eb3c4c857a53f634be24ebe77fd7f1ac3ee0a3e
ampawd <arammisampawd@yahoo.com>
2017-10-07 22:33:50 +0400

    project structure reorganization

---
+++ profiler/profiler.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+
+
+using namespace std::chrono;
+
+namespace parallelizm 
+{
+	static double runtime = 0.0f;
+
+	static high_resolution_clock::time_point start, end;
+
+
+	template< class Func, class Container, class ResultType >
+	static void profile(Func func, Container cont, ResultType& res)
+	{
+		std::cout << "profiling started ... " << std::endl;
+		
+		start = high_resolution_clock::now();
+		res = func(cont);
+		end = high_resolution_clock::now();
+		runtime = duration<double>(end - start).count();
+		
+		std::cout << "elapsed time = " << runtime << " sec" << std::endl;
+		std::cout << res << std::endl;
+	}
+
+	template< class Func, class ResultType >
+	static void profile(Func func, ResultType& res)
+	{
+		std::cout << "profiling started ... " << std::endl;
+		
+		start = high_resolution_clock::now();
+		res = func();
+		end = high_resolution_clock::now();
+		runtime = duration<double>(end - start).count();
+		
+		std::cout << "elapsed time = " << runtime << " sec" << std::endl;
+		std::cout << res << std::endl;
+	}
+
+};
